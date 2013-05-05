209af37f39d892ed2684caafffead6b64b222b76
Valent <valent.turkovic@gmail.com>
2013-05-05 23:36:14 +0200

    + ‘rpi-temp.sh’

---
+++ rpi-temp.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+
+# sen.se
+sense_apikey=
+rpigpu_feed=
+rpicpu_feed=
+
+GPUOUT=`/opt/vc/bin/vcgencmd measure_temp`
+CPUOUT=`cat /sys/class/thermal/thermal_zone0/temp`
+
+GPU=`cut -c6-9 <<< $GPUOUT`
+CPU=`cut -c1-2 <<< $CPUOUT`.`cut -c3-5 <<< $CPUOUT`
+#CPU=`printf "%.1f\n" $CPU2`
+
+curl 'http://api.sen.se/events/?sense_key='$sense_apikey -X POST -H "Content-type: application/json" -d '[{"feed_id": '$rpigpu_feed',"value": '$GPU'}]'
+curl 'http://api.sen.se/events/?sense_key='$sense_apikey -X POST -H "Content-type: application/json" -d '[{"feed_id": '$rpicpu_feed',"value": '$CPU'}]'
+

1def63626f116536dd66315ab3ccc97da5a8d2dc
Valent Turkovic <valent.turkovic@gmail.com>
2014-06-21 17:53:27 +0200

    update
    add "-c /root/.digitemprc" so now running temp-to-sense works from any folder, previously if .dititemprc wasn't located in current directory command would fail

--- temp-to-sense.sh
+++ temp-to-sense.sh
@@ -6,7 +6,7 @@
 apikey=
 roomfeed=
 
-roomtemp=$(digitemp_DS9097 -a -q -o"%.2C")
+roomtemp=$(digitemp_DS9097 -a -q -o"%.2C" -c /root/.digitemprc)
 
 # post to open.sen.se
 curl 'http://api.sen.se/events/?sense_key='$apikey -X POST -H "Content-type: application/json" -d '[{"feed_id": '$roomfeed',"value": '$roomtemp'}]'
